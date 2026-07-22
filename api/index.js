67956ee0f49a2844c7b37d68ff7b98db9f9501b5
Miran Sardar Mohammed <miran.sardar@highperformanceco.net>
2026-07-22 13:06:08 -0700

    Fix stray p causing API crash

--- api/index.js
+++ api/index.js
@@ -129,7 +129,7 @@ app.post('/api/auth/mfa/setup', authMiddleware(), async (req, res) => {
   }
 });
 
-p.post('/api/auth/mfa/verify', authMiddleware(), async (req, res) => {
+post('/api/auth/mfa/verify', authMiddleware(), async (req, res) => {
   try {
     const { code } = req.body;
 
